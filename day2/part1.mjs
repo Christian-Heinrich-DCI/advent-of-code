b07cfa0c00a5dd2e9f01fa07f0831db7ff632de2
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2024-12-11 15:30:53 +0100

    final state for today

--- day2/part1.mjs
+++ day2/part1.mjs
@@ -1,13 +1,30 @@
+import { log } from "node:console";
 import fs from "node:fs";
+import { CLIENT_RENEG_LIMIT } from "node:tls";
 
 try {
   // reading file
   // let data = fs.readFileSync("day1-test-input.txt", "utf8");
   let data = fs.readFileSync("day2-test-input.txt", "utf8");
-  data = data.replaceAll("\r", "");
-  //   const dataArray = data.split("\n");
+  data = data.replaceAll("\r", ""); // go away Windows!
+  const dataArray = data.split("\n");
 
-  console.log(data);
+  console.log(dataArray);
+
+  /** Thoughts:
+   * remember last change
+   * check difference between current and next
+   * compare with last change
+   * decision if safe or not
+   * */
+
+  dataArray.forEach((levelsString) => {
+    const levels = levelsString.split(" ");
+    levels.forEach((level) => {
+      console.log(Number(level));
+    });
+    console.log("---");
+  });
 
   // hopefully the right number???
   console.log();
