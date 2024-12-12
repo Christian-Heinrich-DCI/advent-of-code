d028532cc1d87ea64adf6f8cb64cce23b925fa3b
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2024-12-12 16:13:33 +0100

    endstand für do. / ergebnis zu niedrig

--- day2/part1.mjs
+++ day2/part1.mjs
@@ -1,11 +1,9 @@
-import { log } from "node:console";
 import fs from "node:fs";
-import { CLIENT_RENEG_LIMIT } from "node:tls";
 
 try {
   // reading file
-  // let data = fs.readFileSync("day1-test-input.txt", "utf8");
-  let data = fs.readFileSync("day2-test-input.txt", "utf8");
+  // let data = fs.readFileSync("day2-test-input.txt", "utf8");
+  let data = fs.readFileSync("day2-input.txt", "utf8");
   data = data.replaceAll("\r", ""); // go away Windows!
   const dataArray = data.split("\n");
 
@@ -18,16 +16,49 @@ try {
    * decision if safe or not
    * */
 
+  let numOfSafeReports = 0;
+
   dataArray.forEach((levelsString) => {
     const levels = levelsString.split(" ");
-    levels.forEach((level) => {
-      console.log(Number(level));
-    });
-    console.log("---");
+
+    // establish direction of change
+    let direction = 0; // start - 0, raising: 1, falling: -1
+    if (levels[0] < levels[1]) direction = 1;
+    else if (levels[0] > levels[1]) direction = -1;
+
+    // test from second element, until second to last
+    // first was tested above, last doesn't have a next
+    const safeArray = [];
+    for (let i = 1; i < levels.length - 1; i++) {
+      const current = levels[i];
+      const next = levels[i + 1];
+
+      if (
+        direction === -1 &&
+        current > next &&
+        current - next >= 1 &&
+        current - next <= 3
+      )
+        safeArray.push(true);
+      else if (
+        direction === 1 &&
+        current < next &&
+        next - current >= 1 &&
+        next - current <= 3
+      )
+        safeArray.push(true);
+      else safeArray.push(false);
+    }
+    console.log(safeArray);
+
+    // if there is any false (not safe) in the progression of the levels
+    // of a report, the whole report is unsafe
+    // if no false is found, the report as a whole is safe
+    if (safeArray.indexOf(false) === -1) numOfSafeReports++;
   });
 
   // hopefully the right number???
-  console.log();
+  console.log(numOfSafeReports);
 } catch (err) {
   console.error(err);
 }
